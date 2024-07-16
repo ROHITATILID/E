c91d63bd2bd7150aa121b195994dcaf4dc2b9362
ROHITATILID <rohitpatil5030@gmail.com>
2024-07-16 22:36:08 +0530

    Add files via upload

---
+++ javapalimdrome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class javapalimdrome {
+	public static void main (String[]args) {
+	      int n,s=0,c,r;
+	      System.out.println("Enter any number");
+	      
+	      Scanner ref=new Scanner(System.in);
+	    		  n=ref.nextInt();
+	    		  c=n;
+	    		  while(n>0) {
+	    		 r=n%10;
+	    		 s=(s*10)+r;
+	    		 n=n/10;
+	    		  }
+	    		 
+	if(c==s) {
+		System.out.println("Palimdrom number");//11111 Palimdrom number
+                                                 
+	}
+	else {
+		System.out.println("Not Palimdrom number");
+	
+		
+	}
+
+}
+}
